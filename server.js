b90e90b4b07994c0f5a2cd5c1afa450068baef1c
Sergio Bruni <sbruni@socialh4ck.com>
2016-06-20 18:14:00 -0400

    added cron job

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var moment  = require('moment');
 var _       = require('lodash');
 var fs      = require('fs');
 var app     = express();
+var CronJob = require('cron').CronJob;
+
 
 app.listen('8085')
 
@@ -13,52 +15,66 @@ var jsonbuilds = {builds: {}, last_updated: ''};
 
 var characterJson = ["Abathur", "Anub'arak", "Artanis", "Arthas", "Azmodan", "Brightwing", "Chen", "Cho", "Chromie", "Dehaka", "Diablo", "E.T.C.", "Falstad", "Gall", "Gazlowe", "Greymane", "Illidan", "Jaina", "Johanna", "Kael'thas", "Kerrigan", "Kharazim", "Leoric", "Li Li", "Li-Ming", "Lt. Morales", "Lunara", "Malfurion", "Medivh", "Muradin", "Murky", "Nazeebo", "Nova", "Raynor", "Rehgar", "Rexxar", "Sgt. Hammer", "Sonya", "Stitches", "Sylvanas", "Tassadar", "The Butcher", "The Lost Vikings", "Thrall", "Tracer", "Tychus", "Tyrael", "Tyrande", "Uther", "Valla", "Xul", "Zagara", "Zeratul"];
 
-jsonbuilds.last_updated = moment().format('MMMM Do YYYY');
+jsonbuilds.last_updated = moment().format('MMMM Do YYYY, h:mm a');
 
-_.forEach(characterJson, function (value, key)  {
+var job = new CronJob({
+  cronTime: '0 40 17 * * 3',
+  onTick: function() {
+    /*
+     * Runs every Wednesday
+     * at 06:00:00 AM. 
+     */
+    console.log('Cron Job Started ', moment().format('LLL'))
 
-  var url = 'http://www.hotslogs.com/Sitewide/HeroDetails?Hero=' + value
-
-  var model = 
-    {
-      build1: { 
-        lv01: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent01', get: 'title' },
-        lv04: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent04', get: 'title' },
-        lv07: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent07', get: 'title' },
-        lv10: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent10', get: 'title' },
-        lv13: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent13', get: 'title' },
-        lv16: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent16', get: 'title' },
-        lv20: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent20', get: 'title' }
-      },
-      build2: { 
-        lv01: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent01', get: 'title' },
-        lv04: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent04', get: 'title' },
-        lv07: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent07', get: 'title' },
-        lv10: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent10', get: 'title' },
-        lv13: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent13', get: 'title' },
-        lv16: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent16', get: 'title' },
-        lv20: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent20', get: 'title' }
-      }
-    }
-  
-  scrapy.scrape(url, model, function(err, data) {
-      if (err) return console.error(err)
-
-      jsonbuilds.builds[value] = data
-
-      console.log(Object.size(jsonbuilds.builds), characterJson.length);
-
-      if (Object.size(jsonbuilds.builds) === characterJson.length){
+    _.forEach(characterJson, function (value, key)  {
       
-        fs.writeFile('hotsbuilds.json', JSON.stringify(jsonbuilds, null, 4), function(err){
-          console.log('File successfully written! - Check your project directory for the hotsbuilds.json file');
-        })
+      var url = 'http://www.hotslogs.com/Sitewide/HeroDetails?Hero=' + value
+      var model = 
+          {
+            build1: { 
+              lv01: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent01', get: 'title' },
+              lv04: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent04', get: 'title' },
+              lv07: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent07', get: 'title' },
+              lv10: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent10', get: 'title' },
+              lv13: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent13', get: 'title' },
+              lv16: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent16', get: 'title' },
+              lv20: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl04_imgTalent20', get: 'title' }
+            },
+            build2: { 
+              lv01: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent01', get: 'title' },
+              lv04: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent04', get: 'title' },
+              lv07: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent07', get: 'title' },
+              lv10: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent10', get: 'title' },
+              lv13: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent13', get: 'title' },
+              lv16: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent16', get: 'title' },
+              lv20: { selector: '#ctl00_MainContent_RadGridPopularTalentBuilds_ctl00_ctl06_imgTalent20', get: 'title' }
+            }
+          }
 
-      }
-  });
-  
+      scrapy.scrape(url, model, function(err, data) {
+          if (err) return console.error(err)
 
-})
+          jsonbuilds.builds[value] = data
+
+          console.log(Object.size(jsonbuilds.builds), characterJson.length);
+
+          if (Object.size(jsonbuilds.builds) === characterJson.length){
+          
+            fs.writeFile('hotsbuilds.json', JSON.stringify(jsonbuilds, null, 4), function(err){
+              console.log('File successfully written!');
+            })
+
+          }
+      });
+    })
+
+  },
+  start: false,
+  runOnInit: true,
+  timeZone: 'America/Caracas'
+});
+
+job.start();
 
 Object.size = function(obj) {
     var size = 0, key;
@@ -67,6 +83,5 @@ Object.size = function(obj) {
     }
     return size;
 };
-
 
 exports = module.exports = app;
